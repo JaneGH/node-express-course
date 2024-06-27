2bd9f5a5baa62d3f97b81ffd180d4698c3db5bd8
JaneGH <jkrytsyna@gmail.com>
2024-06-27 12:52:16 +0200

    fixed mistakes

--- 01-node-tutorial/answers/writeWithPromisesThen.js
+++ 01-node-tutorial/answers/writeWithPromisesThen.js
@@ -1,18 +1,18 @@
-const fs = require('fs');
+const { writeFile, appendFile, readFile } = require('fs').promises;
 
 // Write to temp.txt
-fs.promises.writeFile('temp.txt', 'Line 1\n')
+writeFile('temp.txt', 'Line 1\n')
     .then(() => {
-        console.log('First line written.txt');
-        return fs.promises.appendFile('temp.txt', 'Line 2\n');
+        console.log('First line written');
+        return appendFile('temp.txt', 'Line 2\n');
     })
     .then(() => {
-        console.log('Second line written.txt');
-        return fs.promises.appendFile('temp.txt', 'Line 3\n');
+        console.log('Second line written');
+        return appendFile('temp.txt', 'Line 3\n');
     })
     .then(() => {
-        console.log('Third line written.txt');
-        return fs.promises.readFile('temp.txt', 'utf8');
+        console.log('Third line written');
+        return readFile('temp.txt', 'utf8');
     })
     .then((data) => {
         console.log('Content of temp.txt:');
