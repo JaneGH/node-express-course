166c52b8c79f7f9289e3dc1d48923ad017b9b946
JaneGH <jkrytsyna@gmail.com>
2024-07-09 19:31:54 +0200

    divided into week 4 and week 5

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -3,21 +3,20 @@ const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
-const notFound = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
 
-// middleware
+//middleware
+app.use(express.json())
 
-app.use(express.static('./public'));
-app.use(express.json());
+//routes
+app.get('/hello', (req, res) => {
+  res.send('Task Manager App');
 
-// routes
+});
+
 
 app.use('/api/v1/tasks', tasks);
 
-app.use(notFound);
-app.use(errorHandlerMiddleware);
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
